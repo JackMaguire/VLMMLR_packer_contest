740ec44e088e6c01328c435a8a8531d4f65da4bd
Jack Maguire <jackmaguire1444@gmail.com>
2019-10-14 10:30:10 -0400

    NoAAInfo/analyze.py

--- NoAAInfo/read_vlmmlr_data.py
+++ NoAAInfo/read_vlmmlr_data.py
@@ -1,12 +1,1 @@
-import numpy
-import pandas as pd
-
-numpy.random.seed( 0 )
-
-def read_from_file( filename ):
-    data = pd.read_csv( filename, header=None ).values
-    amino_acids = data[:,0:1]
-    input_data = data[:,1:16]
-    output = data[:,16:17]
-    return input_data, output
-
+../read_vlmmlr_data.py
